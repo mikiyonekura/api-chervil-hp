7713be38f8952e737b546a032d91f739f407c64e
yonekuramiki <yonekuramiki@yonekuramikinoMacBook-Pro.local>
2023-08-11 02:39:11 +0900

    Change port setting

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+app.run(debug=False, host='0.0.0.0', port=8080)
+
+
 @app.route('/hello', methods=["GET"])
 def hello_world():
     return jsonify({'message': 'Hello, world'})
