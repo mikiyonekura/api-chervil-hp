a0a4c6544f7fc1b34665545b9a57647b41c478ba
yonekuramiki <yonekuramiki@yonekuramikinoMacBook-Pro.local>
2023-08-11 02:43:47 +0900

    Change port setting

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
-
-app.run(debug=False, host='0.0.0.0', port=8080)
 
 
 @app.route('/hello', methods=["GET"])
@@ -10,4 +9,5 @@ def hello_world():
     return jsonify({'message': 'Hello, world'})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
